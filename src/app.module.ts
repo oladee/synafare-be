74b143b97232a6b2c2146bd4f1eef3a43fc45296
oladee <deemajor230600@gmail.com>
2025-07-29 23:17:28 +0100

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, FirebaseModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal  : true
+  }),MongooseModule.forRootAsync({
+    imports : [ConfigModule],
+    useFactory : (config:ConfigService)=>({
+      uri : config.get("NODE_ENV") === "dev" ? config.get("MONGO_TEST_URL") : config.get("MONGO_PROD_URL")
+    }),
+    inject :[ConfigService]
+  }),UserModule, FirebaseModule, AuthModule],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Types } from 'mongoose';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
+  @Get('allusers')
+  findAll(@Query('filter') filter?: string,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10) {
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+    const sanitizedLimit = Math.min(Math.max(+limit || 10, 1), 100); // max 100 users per page
+    const sanitizedPage = Math.max(+page || 1, 1);
+    return this.userService.findAll({ filter, page: sanitizedPage, limit: sanitizedLimit });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  findOne(@Param('id') id: Types.ObjectId) {
+    return this.userService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
 }
