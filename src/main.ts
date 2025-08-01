ed142c9dc41c77b587a93a7e3639b141ed387b3a
oladee <deemajor230600@gmail.com>
2025-08-01 14:50:24 +0100

    cors inclusino

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     enableImplicitConversion: true, // <- This line here
   },}))
 
-  app.enableCors({origin : ['http://localhost:3000','http://localhost:3001','http://localhost:5173'], credentials : true})
+  app.enableCors({origin : ['http://localhost:3000','http://localhost:3001','http://localhost:5173','https://synafare-fe.vercel.app'], credentials : true})
 
   app.use(cookieParser(process.env.COOKIE_SECRET))
   await app.listen(process.env.PORT ?? 3000);
