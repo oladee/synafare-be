be6bfe5b7208bcdfb8013363dcd10a142eded09c
Oladimeji Momoh <deemajor230600@gmail.com>
2025-08-16 07:32:31 +0100

    Update loan.service.ts

--- src/user/loan.service.ts
+++ src/user/loan.service.ts
@@ -37,6 +37,7 @@ export class LoanService {
       const [loans, total] = await Promise.all([
         this.loanModel.find(query)
         .populate('customer')
+        .populate('user)
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
@@ -346,6 +347,7 @@ export class LoanService {
       const [loans, total, total_accepted, total_declined] = await Promise.all([
         this.loanModel.find(query)
         .populate('customer')
+        .populate('user)
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
